c97cd33489b46461fef57a09877613ab4f17f905
Fernando Rodriguez <ferchulobo2015@gmail.com>
2024-05-26 19:25:39 -0300

    correcion

--- assets/js/validacion.js
+++ assets/js/validacion.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Mostrar un mensaje de error o redirigir a la página de inicio de sesión
                 alert('Debes iniciar sesión para acceder a la página de inicio.');
                 // Podrías redirigir al usuario a la página de inicio de sesión en lugar de mostrar una alerta
+                window.location.href = './pages/registrarse.html'; // Redirigir al formulario de registro
             }
         });
     });
